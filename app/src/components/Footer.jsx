ff3918762ce013072f38b0bd5bb709bc48518560
João Vitor Ezequiel <joaoezeki@gmail.com>
2023-03-24 19:14:28 -0300

    communicate APIs v16

--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -1,9 +1,13 @@
-
+import '../assets/footer.css'
 
 export const Footer = () => {
   return (
     <div className="Footer">
-      <p>Feito por: João Vitor</p>
+      <p className='logo'>&lt;JVE/&gt;</p>
+      <div className='contato'>
+        <p>Feito por: João Vitor</p>
+        <a href="https://github.com/joaovitor8">GitHub</a>
+      </div>
     </div>
   )
 }
