909d28f7d55b1ed63ec8c79bbd60a89f876e159f
João Vitor Ezequiel <joaoezeki@gmail.com>
2023-03-30 17:37:34 -0300

    communicate APIs v16.1

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,18 @@ const express = require('express')
 const app = express()
 
 
+app.get('/', (req, res) => {
+  res.send('Pagina principal')
+})
+
+app.get('/sobre', (req, res) => {
+  res.send('Pagina sobre')
+})
+
+app.get('/blog', (req, res) => {
+  res.send('Pagina blog')
+})
+
 
 app.listen(8888, () => {console.log('Servidor rodando na porta http://lacalhost:8888')})
 
