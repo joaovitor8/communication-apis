6c6df2ccbe09632431d31b5c6c7b91bdf03939a3
João Vitor <joaoezeki@gmail.com>
2023-05-27 19:28:48 -0300

    pag cat concluido

--- web/src/app/(pages)/cat/page.tsx
+++ web/src/app/(pages)/cat/page.tsx
@@ -1,19 +1,26 @@
+/* eslint-disable @next/next/no-img-element */
 'use client'
 
-import { CatApi } from "@/app/api/api"
-import { useState } from "react"
+import { useState } from 'react'
 
 export default function Cat() {
-  const [pegarCat, setPegarCat] = useState()
+  const [numberCat, setNumberCat] = useState<string>('')
+  const [imgCat, setImgCat] = useState<number>(0)
 
+  const Buscar = () => {
+    const n = parseInt(numberCat)
+    setImgCat(n)
+  }
 
   return (
     <div className="flex h-screen flex-col items-center space-y-3">
       <div className="flex h-24 w-4/5 items-center justify-center space-x-3 bg-slate-400">
-        <input type="text" className="px-10 py-2" />
-        <button className="rounded-sm bg-green-500 px-4 py-2">Buscar</button>
+        <input type="text" className="px-10 py-2" onChange={(e) => setNumberCat(e.currentTarget.value)}/>
+        <button className="rounded-sm bg-green-500 px-4 py-2" onClick={Buscar}>Buscar</button>
       </div>
-      <div className="h-80 w-4/5 bg-sky-400 "></div>
+      <div className="flex h-96 w-4/5 items-center justify-center bg-sky-400">
+        <img src={`https://http.cat/${imgCat}`} alt="Foto de Gato" className="h-80"/>
+      </div>
     </div>
   )
 }
--- web/src/app/(pages)/dog/page.tsx
+++ web/src/app/(pages)/dog/page.tsx
@@ -1,8 +1,23 @@
+'use client'
+
+import { useEffect, useState } from "react"
+import axios from 'axios'
+
+
 export default function Dog() {
+  const [dogs, setDogs] = useState<string>('')
+
+  const Buscar = () => {}
+
+  useEffect(() => {
+    axios.get('https://random.dog/woof')
+    .then(res => console.log(res.data))
+  }, [])
+
   return (
     <div className="flex h-screen flex-col items-center space-y-3">
       <div className="flex h-24 w-4/5 items-center justify-center space-x-3 bg-slate-400">
-        <button className="rounded-sm bg-green-500 px-10 py-3 uppercase">Buscar</button>
+        <button className="rounded-sm bg-green-500 px-10 py-3 uppercase" onClick={Buscar}>Buscar</button>
       </div>
       <div className="h-80 w-4/5 bg-sky-400 "></div>
     </div>
