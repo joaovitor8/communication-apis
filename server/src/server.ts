27f95c6f979ed92190d56c6a0580a6cc865aa158
João Vitor <joaoezeki@gmail.com>
2023-06-09 21:13:29 -0300

    tentando conectar mongoDB

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,32 +1,32 @@
-import fastify from 'fastify'
+const fastify = require("fastify")()
 
-const app = fastify()
 
-// Banco de Dados improvisado
-const DB: any[] = []
-
-// Criar um usuário
-app.post('/user', async (request: any, reply: any) => {
-  const user = request.body
-  DB.push(user)
-  reply.send(user)
+fastify.register(require("@fastify/mongodb"), {
+  forceClose: true,
+  url: "mongodb://localhost:27017/teste"
 })
 
+//-----------------------------------------------
+
+// Criar um usuário
+fastify.post('/users', async () => {})
+
 // Pegar todos os usuários
-app.get('/user', async (request: any, reply: any) => {
-  reply.send(DB)
+fastify.get('/users', async () => {
+  fastify.mongo.client.db('mydb').collection('users')
+  //const users = fastify.mongo.db.collection('users')
 })
 
 // Pegar um usuário
-app.get('/user/:id', async (request: any, reply: any) => {})
+fastify.get('/users/:id', async () => {})
 
 // Atualizar um usuário
-app.put('/user/:id', async (request: any, reply: any) => {})
+fastify.put('/users/:id', async () => {})
 
 // Deletar um usuário
-app.delete('/user/:id', async (request: any, reply: any) => {})
+fastify.delete('/users/:id', async () => {})
+
+//-----------------------------------------------
 
 // Iniciar Servidor
-app.listen({
- port: 3333
-}).then(() => { console.log('Servidor rodando na porta: http://localhost:3333')})
+fastify.listen({ port: 3333 }).then(() => { console.log('Servidor rodando na porta: http://localhost:3333') })
